2656685da5c2fa1c926dd5e8d665c26c791af1be
Dominik Spitzli <dominik@spitzli.dev>
2023-02-26 23:00:02 +0100

    added a type

--- migrations/20230220171821_create_settings.up.sql
+++ migrations/20230220171821_create_settings.up.sql
@@ -4,6 +4,7 @@ CREATE TABLE settings (
   guild_id bigint not null,
   name text not null,
   value text not null,
+  type text not null,
   created_at timestamp not null default now(),
   updated_at timestamp not null default now()
 );
