523b616d465f4cc5ef8129e7aa1372da7560e2ef
Dominik Spitzli <dominik@spitzli.dev>
2023-03-23 23:37:46 +0100

    will be deleted soon

--- migrations/20230220171821_create_settings.up.sql
+++ migrations/20230220171821_create_settings.up.sql
@@ -1,10 +1,10 @@
 -- Add up migration script here
 CREATE TABLE settings (
-  id serial primary key,
   guild_id bigint not null,
   name text not null,
-  value text not null,
-  type text not null,
+  value jsonb not null,
   created_at timestamp not null default now(),
   updated_at timestamp not null default now()
 );
+
+CREATE UNIQUE INDEX settings_guild_id_name_unique ON settings (guild_id, name);
