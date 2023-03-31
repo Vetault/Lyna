b504be3d03825112f45d608ec66f71a2286a31ef
Dominik Spitzli <dominik@spitzli.dev>
2023-03-31 02:07:41 +0200

    update env check

--- build.rs
+++ build.rs
@@ -5,18 +5,18 @@ use tokio::runtime::Runtime;
 
 fn main() -> Result<()> {
     println!("cargo:rerun-if-changed=migrations");
-    let rt = Runtime::new().unwrap();
-    rt.block_on(async move {
-        let pool = PgPoolOptions::new()
-            .connect(&dotenvy::var("DATABASE_URL").unwrap())
-            .await
-            .unwrap();
 
-        let m = Migrator::new(std::path::Path::new("./migrations"))
-            .await
-            .unwrap();
-        m.run(&pool).await.unwrap();
-    });
+    if let Ok(database_url) = dotenvy::var("DATABASE_URL") {
+        let rt = Runtime::new().unwrap();
+        rt.block_on(async move {
+            let pool = PgPoolOptions::new().connect(&database_url).await.unwrap();
+
+            let m = Migrator::new(std::path::Path::new("./migrations"))
+                .await
+                .unwrap();
+            m.run(&pool).await.unwrap();
+        });
+    }
 
     rosetta_build::config()
         .source("bg", "locales/bg.json")
